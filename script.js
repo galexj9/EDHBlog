62de1b58f6e942518c3f038ab8ffb76675eadb11
galexj9 <galexj9@gmail.com>
2022-10-27 15:09:00 -0500

    added navbar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function randomColorBackground(element) {
     var color = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.getElementById(element).style.backgroundColor = color;
+    document.getElementById(element).innerText = ":D";
   }
 
   function randomColorBorder(element) {
